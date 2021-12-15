ad85357a870c23f211c7034f79b5c879001c7844
DanielmPC <dleonm1902@alumno.ipn.mx>
2021-12-15 14:49:00 -0600

    Actualización del juego :fire:

--- Copia de seguridad.py
+++ Copia de seguridad.py
@@ -12,36 +12,45 @@ m = 0
 
 while(bool(a)==False):
 
-    num1 = int(input("Ingrese un número entero: "))
-    lista.append(num1)
+    num1 = input("Ingrese un número entero: ")
 
-    if num1 != num:
-
-        if num1 > num:
-            bool(1)
-            maximo.append(num1)
-            M = min(maximo)
-            print("\033[1;31m" + f"Error, el número está entre {M} y {m}")
-            #print("\033[1;31m" + f"El número es incorrecto, está entre [{M}, {m}]")
-            i += 1
-
-        if num1 < num:
-            minimo.append(num1)
-            m = max(minimo)
-            print("\033[1;31m" + f"Error, el número está entre {M} y {m}")
-            #print("\033[1;31m" + f"El número está entre [{M}, {m}], elige nuevamente: ")
-            i += 1
-
+    if num1.isnumeric() != True:
+        print("Ingrese un valor numerico")
+        num1 = input("Ingrese un número entero: ")
     
     else:
+        num1 = int(num1)
+        lista.append(num1)
 
-        if i == 1:
-            bool(1)
-            print("\033[1;31m" + f">>>>Correcto, el  número es {num}, lo realizó en 1 intento.")
+        if num1 != num:
+
+            if num1 > num:
+                bool(1)
+                maximo.append(num1)
+                M = min(maximo)
+                print("\033[1;31m" + f"Error, el número está entre {M} y {m}")
+                #print("\033[1;31m" + f"El número es incorrecto, está entre [{M}, {m}]")
+                i += 1
+
+            if num1 < num:
+                minimo.append(num1)
+                m = max(minimo)
+                print("\033[1;31m" + f"Error, el número está entre {M} y {m}")
+                #print("\033[1;31m" + f"El número está entre [{M}, {m}], elige nuevamente: ")
+                i += 1
+
+        
         else:
-            bool(1)
-            print("\033[1;32m" + f">>>>Correcto, el  número es {num}, lo realizó en {i+1} intentos.")
-        a = rd.random()
+
+            if i == 1:
+                bool(1)
+                print("\033[1;31m" + f">>>>Correcto, el  número es {num}, lo realizó en 1 intento.")
+                print(f"Los números que eligió durante el juego son: {lista}")
+            else:
+                bool(1)
+                print("\033[1;32m" + f">>>>Correcto, el  número es {num}, lo realizó en {i+1} intentos.")
+                print(f"Los números que eligió durante el juego son: {lista}")
+            a = rd.random()
 
 """
 Notas:
